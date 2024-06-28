cd5a8d12e925e8952e9709b4991e03dc14f58af3
lana-del-rey <iskandar100@gmail.com>
2024-06-29 01:15:59 +0200

    Stripe test secret

--- amplify/functions/stripe-create-checkout-session/resource.ts
+++ amplify/functions/stripe-create-checkout-session/resource.ts
@@ -1,10 +1,10 @@
-import { defineFunction, secret } from '@aws-amplify/backend';
+import { defineFunction } from '@aws-amplify/backend';
 
 // Define the function
 export const stripeCheckoutHandler = defineFunction({
     entry: './handler.ts',
     environment: {
         FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000',
-        STRIPE_SECRET_KEY: secret('STRIPE_SECRET_KEY'),
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY!,
     },
 })
