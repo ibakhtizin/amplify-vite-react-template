fde7b012639a8a42fdf45712d09f842483807648
lana-del-rey <iskandar100@gmail.com>
2024-06-29 03:18:11 +0200

    Stripe webhook

--- amplify/functions/stripe/resource.ts
+++ amplify/functions/stripe/resource.ts
@@ -4,5 +4,9 @@ export const stripeWebhook = defineFunction({
     // optionally specify a name for the Function (defaults to directory name)
     name: 'stripe-webhook',
     // optionally specify a path to your handler (defaults to "./handler.ts")
-    entry: './stripe-webhook.ts'
+    entry: './stripe-webhook.ts',
+    environment: {
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY!,
+        STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET!,
+    }
 });
--- amplify/functions/stripe/stripe-webhook.ts
+++ amplify/functions/stripe/stripe-webhook.ts
@@ -1,7 +1,39 @@
+import Stripe from 'stripe';
 import type { APIGatewayProxyHandler } from "aws-lambda";
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {apiVersion: '2024-06-20'});
 
 export const handler: APIGatewayProxyHandler = async (event) => {
     console.log("event", event);
+
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+    if (!webhookSecret) {
+        console.error('STRIPE_WEBHOOK_SECRET is not set');
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Webhook secret is not configured' })
+        };
+    }
+
+    const sig = event.headers['stripe-signature']!;
+    const stripeEvent = stripe.webhooks.constructEvent(event.body!, sig, webhookSecret);
+
+    console.log(stripeEvent.type)
+    switch (stripeEvent.type) {
+        case 'checkout.session.completed':
+            console.log(stripeEvent.type)
+            // await handleCheckoutSessionCompleted(stripeEvent.data.object as Stripe.Checkout.Session);
+            break;
+        case 'customer.subscription.updated':
+            console.log(stripeEvent.type)
+            // await handleSubscriptionChange(stripeEvent.data.object as Stripe.Subscription);
+            break;
+        default:
+            console.log(`Unhandled event type ${stripeEvent.type}`);
+    }
+
     return {
         statusCode: 200,
         // Modify the CORS settings below to match your specific requirements
